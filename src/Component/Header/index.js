dad8bf3eb0f3843f09a7fb73bb4cffde2b838010
pavigitup <pavithragokali@gmail.com>
2024-03-08 13:11:33 +0000

    v1

---
+++ src/Component/Header/index.js
@@ -0,0 +1,16 @@
+import {AiFillHome} from 'react-icons/ai'
+import {BiSolidBriefcase} from 'react-icons/bi'
+
+const Header = () => (
+  <div>
+    <div>
+      <img src="" alt="" className="" />
+    </div>
+    <div>
+      <AiFillHome />
+      <BiSolidBriefcase />
+    </div>
+  </div>
+)
+
+export default Header
---
+++ src/Component/Jobs/index.js
@@ -0,0 +1,14 @@
+import {Component} from 'react'
+import './index.css'
+
+class Jobs extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Jobs</h1>
+      </div>
+    )
+  }
+}
+
+export default Jobs
